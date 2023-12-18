4d37019cf1232e603be001bd179082c15defe2b8
Jaehyeon Lee <jaehyeon.lee@navercorp.com>
2023-12-18 15:47:12 +0900

    Update version to 1.0.3

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/naver/realtime-tech-webrtc-apple-releases/releases/download/1.0.1/WebRTC.xcframework.zip",
-            checksum: "016f1d4cc30826e49ac16657470d6440f2e6b670ee0281b18df4e6f24e1abb15"
+            url: "https://github.com/naver/realtime-tech-webrtc-apple-releases/releases/download/1.0.3/WebRTC.xcframework.zip",
+            checksum: "d7823e7e013e32b7fc2d27ad594b49ee25c093678bccff93c593e7cbccd3b8c3"
         )
     ]
 )
