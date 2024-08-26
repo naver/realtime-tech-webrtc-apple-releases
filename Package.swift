77deb9c048fa63332605254b8973b04178ec2480
hyunsikwon <hyunsik.won@navercorp.com>
2024-08-26 13:38:18 +0900

    Update version to 1.0.12

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/naver/realtime-tech-webrtc-apple-releases/releases/download/1.0.11/WebRTC.xcframework.zip",
-            checksum: "b104ddb78128a1241fd1cf8475f1065358dc2f1d4509fb3b8be5a5989800d85c"
+            url: "https://github.com/naver/realtime-tech-webrtc-apple-releases/releases/download/1.0.12/WebRTC.xcframework.zip",
+            checksum: "4a07a5dd98545f78b26d8c1a8eb1d1ddc94bc0ab8882ccb96986c63d2eabf0b2"
         )
     ]
 )
